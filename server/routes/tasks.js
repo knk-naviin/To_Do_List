885cee3f6d4ef0127067d682a11c7a894b5df952
NAVEENKUMAR K <knknaveen28@gmail.com>
2024-10-25 23:38:02 +0530

    commit

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const { auth } = require("../middlewares/auth");
-// const {
-//   getTasks,
-//   createTask,
-//   updateTask,
-//   deleteTask,
-// } = require("../controllers/taskController");
-
-// const router = express.Router();
-
-// router.get("/", auth, getTasks);
-// router.post("/", auth, createTask);
-// router.put("/:id", auth, updateTask);
-// router.delete("/:id", auth, deleteTask);
-
-// module.exports = router;
 const express = require("express");
 const {
   createTask,
